1989092832e679477274f9ec6e05132722469aa4
heyipomoea <heyipomoea@gmail.com>
2017-06-27 13:24:33 +0800

    Initial commit

---
+++ app/src/main/java/com/example/yvtc/t061502/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.yvtc.t061502;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void click1(View v){
+        TextView tv = (TextView) findViewById(R.id.textView);
+        tv.setText("HelloWorld");
+    }
+
+    public void click2(View v){
+        TextView tv2 = (TextView) findViewById(R.id.textView2);
+        EditText ed = (EditText) findViewById(R.id.editText);
+        String str = ed.getText().toString();
+        tv2.setText("Hello,"+str);
+    }
+
+    public void click3(View v){
+        TextView tv4 = (TextView) findViewById(R.id.textView4);
+        EditText ed2 = (EditText) findViewById(R.id.editText2);
+        EditText ed3 = (EditText) findViewById(R.id.editText3);
+
+        String ans = String.valueOf(Integer.valueOf(ed2.getText().toString()) + Integer.valueOf(ed3.getText().toString()));
+        tv4.setText(ans);
+    }
+}
